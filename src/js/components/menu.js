25e85e82e8bc873b3f8b9b54abd0f664a787c84b
Mikhail Zhyhariev <zhygarevmikhail@gmail.com>
2018-03-03 01:06:22 +0300

    Сверстан компонент Courses

--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -1,12 +1,16 @@
 ;(function() {
-  var menuList = document.querySelector('.menu__list');
-  var menuButton = document.querySelector('.menu__button');
+  try {
+    var menuList = document.querySelector('.menu__list');
+    var menuButton = document.querySelector('.menu__button');
 
-  menuButton.onclick = function() {
-    if (menuList.style.display == 'none') {
-      menuList.style.display = 'flex';
-    } else {
-      menuList.style.display = 'none';
+    menuButton.onclick = function() {
+      if (menuList.style.display == 'none') {
+        menuList.style.display = 'flex';
+      } else {
+        menuList.style.display = 'none';
+      }
     }
+  } catch(err) {
+    console.log(err)
   }
 })();
