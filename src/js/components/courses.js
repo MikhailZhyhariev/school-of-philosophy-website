89ac6b6bc2cae0b1384a5f80049d95f432680b57
Mikhail Zhyhariev <zhygarevmikhail@gmail.com>
2018-03-03 13:53:26 +0300

    Исправлены баги отображения компонентов Menu, Courses

--- src/js/components/courses.js
+++ src/js/components/courses.js
@@ -1,31 +1,32 @@
 ;(function() {
-  var courses = document.querySelectorAll('.courses__item');
+  try {
+    var courses = document.querySelectorAll('.courses__item');
 
-  courses.forEach( function(course) {
-    var button = course.querySelector('.courses__button');
-    var link = course.querySelector('.courses__link');
-    var header = course.querySelector('.courses__header');
+    courses.forEach( function(course) {
+      var button = course.querySelector('.courses__button');
+      var link = course.querySelector('.courses__link');
+      var header = course.querySelector('.courses__header');
 
-    var courseHeight = getComputedStyle(course).height
-    console.log(courseHeight);
-
-    button.addEventListener('click', function() {
-      openMenu(course, link, button);
+      button.addEventListener('click', function() {
+        openMenu(course, link, button);
+      });
+      header.addEventListener('click', function() {
+        openMenu(course, link, button);
+      });
     });
-    header.addEventListener('click', function() {
-      openMenu(course, link, button);
-    });
-  });
 
-  function openMenu(course, link, button) {
-    var courseCoord = course.getBoundingClientRect();
-    var linkCoord = link.getBoundingClientRect();
+    function openMenu(course, link, button) {
+      var courseCoord = course.getBoundingClientRect();
+      var linkCoord = link.getBoundingClientRect();
 
-    button.classList.toggle('courses__button-pressed');
-    if (courseCoord.height == 50) {
-      course.style.height = linkCoord.bottom - courseCoord.top + 20 + 'px';
-    } else {
-      course.style.height = 50 + 'px';
+      button.classList.toggle('courses__button-pressed');
+      if (courseCoord.height == 50) {
+        course.style.height = linkCoord.bottom - courseCoord.top + 20 + 'px';
+      } else {
+        course.style.height = 50 + 'px';
+      }
     }
+  } catch(err) {
+    console.log(err);
   }
 })();
--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -4,11 +4,7 @@
     var menuButton = document.querySelector('.menu__button');
 
     menuButton.onclick = function() {
-      if (menuList.style.display == 'none') {
-        menuList.style.display = 'flex';
-      } else {
-        menuList.style.display = 'none';
-      }
+      menuList.classList.toggle('menu__list-closed');
     }
   } catch(err) {
     console.log(err)
