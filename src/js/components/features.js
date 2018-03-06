a2d22b0ec0abef49cbcb46261c0f3094bca6b1dd
Mikhail Zhyhariev <zhygarevmikhail@gmail.com>
2018-03-06 18:14:16 +0300

    Исправлен баг анимации компонента Features

--- src/js/components/features.js
+++ src/js/components/features.js
@@ -2,16 +2,20 @@
   try {
     var features = document.querySelector('.features');
     var background = features.querySelector('.features__background');
+    var items = features.querySelectorAll('.features__item');
+
+    var featuresTop = features.getBoundingClientRect().top;
 
     timer = setInterval(function() {
       var paddingTop = getComputedStyle(features).paddingTop;
-      var backgroundPosition = background.getBoundingClientRect();
       var lastChildBottom = features.lastChild.getBoundingClientRect().bottom;
 
-      if (backgroundPosition.bottom == lastChildBottom) {
+      var backgroundPosition = background.getBoundingClientRect();
+
+      if (backgroundPosition.bottom >= lastChildBottom) {
         background.style.top = paddingTop;
       } else {
-        background.style.top = backgroundPosition.bottom + 'px';
+        background.style.top = backgroundPosition.bottom - featuresTop + 'px';
       }
     }, 1000);
   } catch(err) {
