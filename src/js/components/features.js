d8d93b7ef3baf636ef4cd6234dba5cba78913d40
Mikhail Zhyhariev <zhygarevmikhail@gmail.com>
2018-03-07 00:01:38 +0300

    Оптимизирована анимация компонента Features

--- src/js/components/features.js
+++ src/js/components/features.js
@@ -5,16 +5,15 @@
     var items = features.querySelectorAll('.features__item');
 
     var featuresTop = features.getBoundingClientRect().top;
-    var items = features.querySelectorAll('.features__item');
 
     timer = setInterval(function() {
       var paddingTop = getComputedStyle(features).paddingTop;
       var active = features.querySelector('div[active]')
       var nextItem = active.nextElementSibling;
 
-      if (nextItem == null) nextItem = items[1];
+      if (nextItem == null) nextItem = background.nextElementSibling;
       var nextItemTop = nextItem.getBoundingClientRect().top;
-      
+
       active.removeAttribute('active');
       nextItem.setAttribute('active', '');
 
