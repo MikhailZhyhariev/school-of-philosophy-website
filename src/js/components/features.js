c5564846476bfae0f23d6a1b3e5145fded5a5f8d
Mikhail Zhyhariev <zhygarevmikhail@gmail.com>
2018-03-06 14:47:30 +0300

    Сверстан компонент Front

--- src/js/components/features.js
+++ src/js/components/features.js
@@ -1,16 +1,20 @@
 ;(function() {
-  var features = document.querySelector('.features');
-  var background = features.querySelector('.features__background');
+  try {
+    var features = document.querySelector('.features');
+    var background = features.querySelector('.features__background');
 
-  timer = setInterval(function() {
-    var paddingTop = getComputedStyle(features).paddingTop;
-    var backgroundPosition = background.getBoundingClientRect();
-    var lastChildBottom = features.lastChild.getBoundingClientRect().bottom;
+    timer = setInterval(function() {
+      var paddingTop = getComputedStyle(features).paddingTop;
+      var backgroundPosition = background.getBoundingClientRect();
+      var lastChildBottom = features.lastChild.getBoundingClientRect().bottom;
 
-    if (backgroundPosition.bottom == lastChildBottom) {
-      background.style.top = paddingTop;
-    } else {
-      background.style.top = backgroundPosition.bottom + 'px';
-    }
-  }, 1000);
+      if (backgroundPosition.bottom == lastChildBottom) {
+        background.style.top = paddingTop;
+      } else {
+        background.style.top = backgroundPosition.bottom + 'px';
+      }
+    }, 1000);
+  } catch(err) {
+    console.log(err);
+  }
 })();
